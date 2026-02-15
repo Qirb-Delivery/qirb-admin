6bf0e9a2fc7712bb47c0271440a5e5c9704e678c
Amanuel Teferi <amanuel.teferi@bazaarvoice.com>
2026-02-15 10:09:10 +0300

    Update API base URL to use environment variable for improved configuration

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api/admin' });
+const api = axios.create({ baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/admin` });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('admin_token');
